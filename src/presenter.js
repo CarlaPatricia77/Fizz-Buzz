dd1d52fdba6ffdab18bf167b9659c46221e9029c
Carla Copa <carla.copacalle@ucb.edu.bo>
2025-09-09 21:18:08 -0400

    feat: conecta lógica FizzBuzz al formulario y resultado en index

--- src/presenter.js
+++ src/presenter.js
@@ -1,15 +1,11 @@
-import sumar from "./sumador";
+import fizzBuzz from "./fizzbuzz.js";
 
-const first = document.querySelector("#primer-numero");
-const second = document.querySelector("#segundo-numero");
-const form = document.querySelector("#sumar-form");
+const input = document.querySelector("#numero");
+const form = document.querySelector("#fizzbuzz-form");
 const div = document.querySelector("#resultado-div");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-
-  const firstNumber = Number.parseInt(first.value);
-  const secondNumber = Number.parseInt(second.value);
-
-  div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+  const number = Number.parseInt(input.value);
+  div.innerHTML = `<p>${fizzBuzz(number)}</p>`;
 });
