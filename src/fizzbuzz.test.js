f0301882aea9a7852f97f19a3efa789afa519eb3
Carla Copa <carla.copacalle@ucb.edu.bo>
2025-09-09 21:14:22 -0400

    test: agrega prueba para retornar 'Fizz' si el número es múltiplo de 3

--- src/fizzbuzz.test.js
+++ src/fizzbuzz.test.js
@@ -4,4 +4,8 @@ describe("FizzBuzz", () => {
   it("debería retornar el mismo número si no es múltiplo de 3 ni de 5", () => {
     expect(fizzBuzz(1)).toBe(1);
   });
+  it("debería retornar 'Fizz' si el número es múltiplo de 3", () => {
+    expect(fizzBuzz(3)).toBe("Fizz");
+    expect(fizzBuzz(6)).toBe("Fizz");
+  });
 });
